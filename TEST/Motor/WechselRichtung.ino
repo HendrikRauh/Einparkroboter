bbbaa4d83d08fefd29f3707eeed1ce845f023190
Hendrik Rauh <114620133+HendrikRauh@users.noreply.github.com>
2022-10-20 14:50:43 +0200

    e

--- TEST/Motor/WechselRichtung.ino
+++ TEST/Motor/WechselRichtung.ino
@@ -1,37 +1,53 @@
+// Anschlüsse für die Motoren
+
+const int motor11 = 9; // Motor 1, Pin 1
+const int motor12 = 10; // Motor 1, Pin 2
+const int motor21 = 11; // Motor 2, Pin 1
+const int motor22 = 12; // Motor 2, Pin 2
+const int motor31 = 8; // Motor 3, Pin 1
+const int motor32 = 7; // Motor 3, Pin 2
+const int motor41 = 6; // Motor 4, Pin 1
+const int motor42 = 5; // Motor 4, Pin 2
+
 void setup() {
-    // defining motor pins
-    // motor 1 pins: 8,9
-    // motor 2 pins: 10,11
-
-    const int motor1Pin1 = 8;
-    const int motor1Pin2 = 9;
-    const int motor2Pin1 = 10;
-    const int motor2Pin2 = 11;
-
-    pinMode(motor1Pin1, OUTPUT);
-    pinMode(motor1Pin2, OUTPUT);
-    pinMode(motor2Pin1, OUTPUT);
-    pinMode(motor2Pin2, OUTPUT);  
+  // put your setup code here, to run once:
+  pinMode(motor11, OUTPUT);
+  pinMode(motor12, OUTPUT);
+  pinMode(motor21, OUTPUT);
+  pinMode(motor22, OUTPUT);
+  pinMode(motor31, OUTPUT);
+  pinMode(motor32, OUTPUT);
+  pinMode(motor41, OUTPUT);
+  pinMode(motor42, OUTPUT);
+  Serial.begin(9600);
 }
 
 void loop() {
-    // motor 1 forward
-    digitalWrite(8, HIGH);
-    digitalWrite(9, LOW);
-
-    // motor 2 forward
-    digitalWrite(10, HIGH);
-    digitalWrite(11, LOW);
-
-    delay(1000);
-
-    // motor 1 backward
-    digitalWrite(8, LOW);
-    digitalWrite(9, HIGH);
-
-    // motor 2 backward
-    digitalWrite(10, LOW);
-    digitalWrite(11, HIGH);
-
-    delay(1000);
+    
+  // Motor 1
+  digitalWrite(motor11, HIGH);
+  digitalWrite(motor12, LOW);
+  // Motor 2
+  digitalWrite(motor21, HIGH);
+  digitalWrite(motor22, LOW);
+  // Motor 3
+  digitalWrite(motor31, HIGH);
+  digitalWrite(motor32, LOW);
+  // Motor 4
+  digitalWrite(motor41, HIGH);
+  digitalWrite(motor42, LOW);
+  delay(1000);
+  // Motor 1
+  digitalWrite(motor11, LOW);
+  digitalWrite(motor12, HIGH);
+  // Motor 2
+  digitalWrite(motor21, LOW);
+  digitalWrite(motor22, HIGH);
+  // Motor 3
+  digitalWrite(motor31, LOW);
+  digitalWrite(motor32, HIGH);
+  // Motor 4
+  digitalWrite(motor41, LOW);
+  digitalWrite(motor42, HIGH);
+  delay(1000);
 }
--- readme.md
+++ readme.md
@@ -3,4 +3,4 @@
 Website: <https://funduino.de/nr-34-motoren-mit-h-bruecke-l298n-ansteuern>
 
 Pins:
-4 bis 12 mit Motoren
+5 bis 12 mit Motoren
