83449fa96a6bb08215f364e8a013dce7b0d7005c
Hendrik Rauh <114620133+HendrikRauh@users.noreply.github.com>
2022-10-21 20:41:49 +0200

    Ultraschall

---
+++ Test/Ultraschall/distanz.ino
@@ -0,0 +1,34 @@
+// Anschlüsse für Ultraschall-Sensor 
+// VCC = 5V
+// GND = GND
+// TRIG = D2
+// ECHO = D3
+
+const int trigPin = 2;
+const int echoPin = 3;
+long duration;
+int distance;
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+}
+
+void loop() {
+  // set the trigPin on HIGH state for 10 micro seconds
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+
+  // read the echoPin, pulseIn() returns the duration (length of the pulse) in microseconds
+  duration = pulseIn(echoPin, HIGH);
+
+  // calculate the distance
+  distance = duration*0.034/2;
+
+  // print the distance on the Serial Monitor
+  Serial.print("Distanz: ");
+  Serial.println(distance);
+  delay(100);
+}
